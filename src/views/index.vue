bfd58b47e836dc6d1df6d130c28028cf834dc025
linchenCai <2412394569@qq.com>
2025-05-13 17:17:00 +0800

    513

--- src/views/index.vue
+++ src/views/index.vue
@@ -9,9 +9,18 @@ const views=[AddCustomer,ListCustomer]
 //声明变量保存当前需要显示的组件名
 const currentComponent=ref(views[0]);
 const menus =ref([])
+
 /*menu组件选中叶子节点触发的函数，参数index：菜单节点的index值，对应数据库菜单节点的id*/
 const  handlerSelect=function(index){
   console.log(index);
+  axios.get("http://localhost:8080/compIndex?id="+index)
+      .then((response)=>{
+        console.log(response);
+        menus.value=views.value[response.data];
+      })
+      .catch((error)=>{
+        console.log(error);
+      })
   //动态该currentComponent赋值,从组件集合views中获取的组件名字
   currentComponent.value=views[index];
 }
@@ -39,11 +48,11 @@ onMounted(function (){
           <!--添加Menu菜单组件-->
           <el-menu
               class="el-menu-vertical-demo" @select="handlerSelect" >
-            <el-sub-menu v-for=" menu in menus" index="menu.id">
+            <el-sub-menu v-for=" menu in menus" :index="String(menu.id)">
               <template #title>
                 <span>{{ menu.label }}</span>
               </template>
-              <el-menu-item   v-for="subMenu in menu.subMenus" :index="subMenu.id">
+              <el-menu-item   v-for="subMenu in menu.subMenus" :index="String(subMenu.id)">
                   {{subMenu.label}}
               </el-menu-item>
             </el-sub-menu>
