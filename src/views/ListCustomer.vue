cb355b1da01b5f8c00d0d947425aca314c0e5701
linchenCai <2412394569@qq.com>
2025-05-14 17:28:26 +0800

    5_14下午添加客户销售过程信息_能够保存到数据库中

--- src/views/ListCustomer.vue
+++ src/views/ListCustomer.vue
@@ -1,6 +1,7 @@
 <script setup>
-import { onMounted, ref } from "vue";
+import {onMounted, reactive, ref} from "vue";
 import axios from "axios";
+import {ElMessage} from "element-plus";
 
 // 定义客户列表集合数据
 const custList = ref([]);
@@ -24,6 +25,68 @@ onMounted(function(){
 function handlerPageChange(value){
   custListQeury(value)
 }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const dialogCustVisible = ref(false)
+//声明客户对象数据，实现表单的双向绑定
+const custForm = reactive({
+  custName: '',
+  address: '',
+  phone: '',
+  custType: '',
+  grade: 1,
+  hisTotal: 0
+});
+
+//定义函数打开客户信息回显的对话框
+/*
+* index 表示修改按钮所在行的所以
+* row 表示修改按钮所在的行的对象
+* */
+function openCustDialog(index,row) {
+  dialogCustVisible.value = true;
+  //console.log(index);
+  //console.log(row);
+  //将点击的行对象数据赋值给表单，进行回显
+  custForm.custName=row.custName;
+  custForm.address=row.address;
+  custForm.phone=row.phone;
+  custForm.grade=row.grade;
+  custForm.custType=row.custType;
+  custForm.id=row.id; //id不能修改，不需要显示
+
+}
+//定义函数提交修改客户信息表单
+function subUpdateCustForm(){
+  axios.post("http://localhost:8080/updateCust",custForm)
+      .then((response)=>{
+        if(response.data.code==200){
+          ElMessage(response.data.msg);
+          //修改成功，关闭对话框
+          dialogCustVisible.value=false;
+          custListQeury(1)//调用分页查询函数，重新刷新视图
+        }else{
+          ElMessage(response.data.msg);
+        }
+      })
+      .catch((error)=>{
+        console.log(error);
+      })
+}
+//定义函数发生客户信息删除的ajax请求
+function deleteCust(id) {
+  axios.get("http://localhost:8080/delCust?id=" + id)
+      .then((response) => {
+        if (response.data.code == 200) {
+          ElMessage(response.data.msg);
+          custListQeury(1)//调用分页查询函数，重新刷新视图
+        } else {
+          ElMessage(response.data.msg);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+}
 </script>
 
 <template>
@@ -37,10 +100,12 @@ function handlerPageChange(value){
     <el-table-column prop="grade" label="客户等级" />
     <el-table-column prop="hisTotal" label="消费总额" />
     <el-table-column fixed="right" label="操作" width="120">
-      <template #default>
+      <template #default="scope">
+        <el-button link type="primary" size="small" @click="deleteCust(scope.row.id)">删除
+        </el-button>
         <el-button link type="primary" size="small"
-        >删除</el-button>
-        <el-button link type="primary" size="small">修改</el-button>
+                   @click="openCustDialog(scope.$index,scope.row)">修改
+        </el-button>
       </template>
     </el-table-column>
 
@@ -50,11 +115,49 @@ function handlerPageChange(value){
   <el-pagination
       small
       background
-      :page-size="3"
+      :page-size="10"
       :pager-count="10"
       layout="prev, pager, next"
       :total="total"
       class="mt-4" @current-change="handlerPageChange"/>
+  <!-- 回显客户信息的对话框 -->
+  <el-dialog
+      v-model="dialogCustVisible"
+      width="80%"
+  >
+    <h2>修改客户信息</h2>
+
+    <!-- 对话框中添加form -->
+    <el-form :model="custForm" label-width="120px">
+      <el-form-item label="客户名称">
+        <el-input v-model="custForm.custName" style="width: 80%"/>
+      </el-form-item>
+      <el-form-item label="联系地址">
+        <el-input v-model="custForm.address" style="width: 80%"/>
+      </el-form-item>
+      <el-form-item label="联系电话">
+        <el-input v-model="custForm.phone" style="width: 80%"/>
+      </el-form-item>
+      <el-form-item label="客户职业">
+        <el-select v-model="custForm.custType" placeholder="请选择职业...." style="width: 80%">
+          <el-option label="保密" value="保密"/>
+          <el-option label="金融" value="金融"/>
+          <el-option label="互联网" value="互联网"/>
+          <el-option label="IT" value="IT"/>
+          <el-option label="能源" value="能源"/>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="客户等级">
+        <el-input v-model="custForm.grade" style="width: 80%"/>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" @click="subUpdateCustForm">保存</el-button>
+        <el-button>取消</el-button>
+      </el-form-item>
+    </el-form>
+
+  </el-dialog>
 </template>
 
 <style scoped>
--- src/views/index.vue
+++ src/views/index.vue
@@ -3,9 +3,10 @@ import AddCustomer from "@/views/AddCustomer.vue"
 import ListCustomer from "@/views/ListCustomer.vue";
 import {markRaw, onMounted, ref, shallowRef} from "vue";
 import axios from "axios";
+import AddSellJh from "@/views/AddSellJh.vue";
 
 //声明数组保存所有组件
-const views=[markRaw(AddCustomer),markRaw(ListCustomer)]
+const views=[markRaw(AddCustomer),markRaw(ListCustomer),,,markRaw(AddSellJh)]
 //声明变量保存当前需要显示的组件名
 const currentComponent=shallowRef(AddCustomer);
 const menus =ref([])
