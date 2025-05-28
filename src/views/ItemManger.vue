b952e5b5ada1b52f3b7c75bf991e971bc612a34a
linchenCai <2412394569@qq.com>
2025-05-28 11:22:57 +0800

    feat(ItemManager): 完善商品信息管理功能
    - 添加了商品名称、编号、种类、库存等信息的输入框
    -增加了品牌、仓库、供应商等下拉选择框
    - 添加了商品图片上传功能
    - 实现了商品信息的保存和清空功能
    - 增加了商品类目树的选择对话框
    - 优化了表单布局和样式

--- src/views/ItemManger.vue
+++ src/views/ItemManger.vue
@@ -10,8 +10,10 @@
     <div style="text-align: left">
       商品图片:<!--  上传商品图片控件  -->
       <el-upload
+          ref="uploadRef"
           action="http://localhost:8080/uploadImg"
-          list-type="picture-card" :auto-upload="true"
+          list-type="picture-card"
+          :auto-upload="true"
           method="post"
           :on-success="handleAvatarSuccess">
         <el-icon>
@@ -36,41 +38,69 @@
     <el-form :model="itemForm" label-width="120px" :inline="true">
 
       <el-form-item label="名称">
-        <el-input v-model="itemForm.id" />
+        <el-input v-model="itemForm.itemName" />
       </el-form-item>
       <el-form-item label="编号">
-        <el-input v-model="itemForm.id" />
+        <el-input v-model="itemForm.itemNumer" />
       </el-form-item>
       <el-form-item label="种类">
-        <el-input v-model="itemForm.id" />
+        <el-input v-model="itemForm.typeId" placeholder="请选择类型"
+                  @click="openTypeDialog"/>
       </el-form-item>
       <el-form-item label="库存">
-        <el-input v-model="itemForm.id" />
+        <el-input v-model="itemForm.store" />
       </el-form-item>
-      <el-form-item label="品牌">
-        <el-input v-model="itemForm.id" />
+      <el-form-item label="品牌" style="width: 34%">
+        <el-select
+            v-model="itemForm.brandId"
+            class="m-2"
+            placeholder="请选择供品牌"
+        >
+          <el-option
+              v-for="item in brandList"
+              :key="item.brandId"
+              :label="item.brandName"
+              :value="item.brandId"
+          />
+        </el-select>
       </el-form-item>
-      <el-form-item label="仓库">
-        <el-input v-model="itemForm.id" />
+      <el-form-item label="仓库" style="width: 34%">
+        <el-select
+            v-model="itemForm.storeId"
+            class="m-2"
+            placeholder="请选择供仓库"
+        >
+          <el-option
+              v-for="item in storeList"
+              :key="item.storeId"
+              :label="item.storeName"
+              :value="item.storeId"
+          />
+        </el-select>
       </el-form-item>
-      <el-form-item label="供应商" style="width: 35%">
-        <el-select v-model="itemForm.id" class="m-2" placeholder="请选择供应商"
-                   size="large">
+      <el-form-item label="供应商" style="width: 34%">
+        <el-select
+            v-model="itemForm.supplyId"
+            class="m-2"
+            placeholder="请选择供应商"
+
+
+        >
           <el-option
               v-for="item in supplyList"
               :key="item.supplyId"
               :label="item.supplyName"
               :value="item.supplyId"
           />
-
         </el-select>
       </el-form-item>
+
       <el-form-item label="产地" style="width: 34%">
         <el-select
-            v-model="itemForm.id"
+            v-model="itemForm.placeId"
             class="m-2"
             placeholder="请选择供产地"
-            size="large"
+
 
         >
           <el-option
@@ -82,11 +112,13 @@
         </el-select>
 
       </el-form-item>
-      <el-form-item label="单位"style="width: 34%">
+      <el-form-item label="单位" style="width: 34%">
         <el-select
-            v-model="itemForm.id"
+            v-model="itemForm.unitId"
             class="m-2"
             placeholder="请选择商品单位"
+
+
         >
           <el-option
               v-for="item in unitList"
@@ -95,24 +127,41 @@
               :value="item.unitId"
           />
         </el-select>
+
       </el-form-item>
       <el-form-item label="进价">
-        <el-input v-model="itemForm.id" />
+        <el-input v-model="itemForm.price" />
       </el-form-item>
       <el-form-item label="售价">
-        <el-input v-model="itemForm.id" />
+        <el-input v-model="itemForm.sellPrice" />
       </el-form-item>
       <el-form-item label="会员价">
-        <el-input v-model="itemForm.id" />
+        <el-input v-model="itemForm.vipPrice" />
       </el-form-item>
       <el-form-item label="详细介绍" style="width: 72%">
-        <el-input v-model="itemForm.id" type="text"/>
+        <el-input v-model="itemForm.itemDesc" type="text"/>
       </el-form-item>
       <el-form-item label="生成日期">
-        <el-input v-model="itemForm.id" />
+        <el-date-picker
+            v-model="itemForm.itemDate"
+            type="date"
+            placeholder="Pick a day"
+            :size="size"
+        />
       </el-form-item>
       <el-form-item label="保质期">
-        <el-input v-model="itemForm.id" />
+        <el-date-picker
+            v-model="itemForm.endDate"
+            type="date"
+            placeholder="Pick a day"
+            :size="size"
+        />
+      </el-form-item>
+
+      <hr/>
+      <el-form-item>
+        <el-button type="primary" @click="saveItemForm">保存</el-button>
+        <el-button>清空</el-button>
       </el-form-item>
     </el-form>
 
@@ -121,6 +170,25 @@
   <el-dialog v-model="dialogVisible">
     <img w-full :src="dialogImageUrl" alt="Preview Image" />
   </el-dialog>
+
+  <!-- 添加选择商品类型的对话框 -->
+  <!-- 添加商品信息的对话框 -->
+  <el-dialog
+      v-model="typeDialog"
+      width="40%">
+    <h2>选择商品类型</h2>
+    <el-tree
+        :props="config"
+        :data="categoryList"
+        default-expand-all
+        node-key="id"
+        ref="treeRef"
+        highlight-current="true"
+        :expand-on-click-node="false"
+        @node-click="handleClickNode">
+    </el-tree>
+    <el-button type="primary" @click="saveType">确认</el-button>
+  </el-dialog>
 </template>
 
 <script setup>
@@ -128,22 +196,49 @@
 import {reactive, ref} from "vue";
 import { Plus ,ZoomIn } from '@element-plus/icons-vue'
 import axios from "axios";
+import {ElMessage} from "element-plus";
+
+
 //定义对话框状态
 const dialogItemVisible = ref(false);
 //声明表单
 const itemForm = reactive({
-  id: 0
+  id: '',
+  itemNumer:'',
+  itemName:'',
+  typeId:'',
+  store:0,
+  brandId:'',
+  storeId:'',
+  supplyId:'',
+  placeId:'',
+  unitId:'',
+  price:'0.0',
+  sellPrice:0.0,
+  vipPrice:'',
+  itemDesc:'',
+  itemDate:'',
+  endDate: '',
+  imgs:'',
+  imgArr:[]
+
 })
 
 //定义函数打开添加商品对话框
 function openItemDialog() {
-  dialogItemVisible.value = true
+  dialogItemVisible.value = true;
   //调用函数加载供应商列表
   loadSupplyList();
   //加载商品产地选项
   loadPlaceList();
   //加载商品单位选项
   queryUnitList();
+  //加载商品品牌选项
+  queryBrandList();
+  //加载仓库选项
+  queryStoreList();
+  //调用函数获得商品编码
+  getItemCode();
 }
 //声明变量保存需要预览的图片路径
 const dialogImageUrl=ref(null);
@@ -157,6 +252,8 @@ function handlePictureCardPreview(file){
 //定义图片上传成功后的回调函数
 function handleAvatarSuccess(path){
   console.log(path)
+  //将响应的图片路径保存表单的imgs数组中
+  itemForm.imgArr.push(path);
 }
 //声明供应商列表
 const supplyList=ref([]);
@@ -191,6 +288,117 @@ function queryUnitList(){
         console.log(error);
       });
 }
+//声明品牌选项集合
+const brandList=ref([]);
+function queryBrandList(){
+  axios.get("http://localhost:8080/brandList")
+      .then((response)=>{
+        brandList.value=response.data;
+      })
+      .catch((error)=>{
+        console.log(error);
+      });
+}
+//声明仓库列表集合
+const storeList=ref([])
+function queryStoreList(){
+  axios.get("http://localhost:8080/storeList")
+      .then((response)=>{
+        storeList.value=response.data;
+      })
+      .catch((error)=>{
+        console.log(error);
+      });
+}
+//定义选择商品类目的对话框状态
+const typeDialog=ref(false);
+function openTypeDialog(){
+  typeDialog.value=true;
+  itemCategoryTree();//打开对话框显示类目树
+}
+
+//////////////////////////////////////////////////////////////////
+//声明tree的配置
+const config={
+  id:'id',
+  label:'label',
+  children:'children'
+};
+//声明tree的引用
+const treeRef=ref(null);
+//声明树节点数据
+const categoryList=ref([]);
+//发送请求加载树
+function itemCategoryTree(){
+  axios.get("http://localhost:8080/categoryList")
+      .then((response)=>{
+        categoryList.value=response.data;
+      })
+      .catch((error)=>{
+        console.log(error);
+      })
+}
+
+var pnode=null;//记录当前选中的节点对象
+//树节点单击的回调函数
+function handleClickNode(node,data){
+  pnode=node;
+}
+//当选择某个类型节点确认关闭对话框
+function saveType(){
+  if(!(pnode && pnode.children.length==0)){
+    ElMessage("必选选中叶子节点........");
+    return;
+  }
+  typeDialog.value=false;
+  itemForm.typeId=pnode.id;
+}
+//向后台发送请求获得商品编码
+function getItemCode(){
+  axios.get("http://localhost:8080/getCode")
+      .then((response)=>{
+        itemForm.itemNumer=response.data;
+      })
+      .catch((error)=>{
+        console.log(error);
+      })
+}
+const uploadRef = ref();
+//发送请求提交form表单
+function saveItemForm(){
+  //处理表单中的数组,将数组转化为字符，赋值给imgs
+  itemForm.imgs=itemForm.imgArr.toString();
+  console.log(itemForm)
+  axios.post("http://localhost:8080/addItem",itemForm)
+      .then((response)=>{
+        if(response.data.code==200){
+          dialogItemVisible.value=false; //关闭对话框
+          // 清空表单数据
+          for (let key in itemForm) {
+            if (Array.isArray(itemForm[key])) {
+              itemForm[key] = [];
+            } else if (typeof itemForm[key] === 'object') {
+              Object.assign(itemForm[key], {});
+            } else {
+              itemForm[key] = '';
+            }
+          }
+          itemForm.store = 0;
+          itemForm.price = '0.0';
+          itemForm.sellPrice = 0.0;
+          itemForm.vipPrice = '';
+          // 清空图片数组和上传控件
+          itemForm.imgArr = [];
+          if (uploadRef.value) {
+            uploadRef.value.clearFiles();
+          }
+        }
+        ElMessage(response.data.msg);
+      })
+      .catch((error)=>{
+        console.log(error);
+      })
+}
 </script>
 
 <style scoped>
@@ -199,6 +407,7 @@ function queryUnitList(){
   height: 178px;
   display: block;
 }
+
 </style>
 
 
@@ -222,5 +431,19 @@ function queryUnitList(){
   width: 178px;
   height: 178px;
   text-align: center;
+}
+
+::v-deep .el-tree-node.is-current > .el-tree-node__content {
+  background-color:darkblue !important; /* 修改为你想要的颜色 */
+}
+
+
+.custom-tree-node {
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 14px;
+  padding-right: 8px;
 }
 </style>
